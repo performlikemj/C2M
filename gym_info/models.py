e9a909e839e8261b74c844b8c85b7b2a4894e0d5
MJ <mikey.j.1228@gmail.com>
2024-07-07 16:07:17 +0900

    Comment out azure storage settings

--- gym_info/models.py
+++ gym_info/models.py
@@ -3,21 +3,21 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import Q
 from django.conf import settings
-from storages.backends.azure_storage import AzureStorage
+# from storages.backends.azure_storage import AzureStorage
 
-class AzureMediaStorage(AzureStorage):
-    account_name = settings.AZURE_ACCOUNT_NAME
-    account_key = settings.AZURE_STORAGE_KEY
-    azure_container = settings.AZURE_MEDIA_CONTAINER
-    expiration_secs = None
-    overwrite_files = True
+# class AzureMediaStorage(AzureStorage):
+#     account_name = settings.AZURE_ACCOUNT_NAME
+#     account_key = settings.AZURE_STORAGE_KEY
+#     azure_container = settings.AZURE_MEDIA_CONTAINER
+#     expiration_secs = None
+#     overwrite_files = True
 
 
-class AzureStaticStorage(AzureStorage):
-    account_name = settings.AZURE_ACCOUNT_NAME
-    account_key = settings.AZURE_STORAGE_KEY
-    azure_container = settings.AZURE_STATIC_CONTAINER
-    expiration_secs = None
+# class AzureStaticStorage(AzureStorage):
+#     account_name = settings.AZURE_ACCOUNT_NAME
+#     account_key = settings.AZURE_STORAGE_KEY
+#     azure_container = settings.AZURE_STATIC_CONTAINER
+#     expiration_secs = None
 
 # Updated Trainer model to link with User
 class Trainer(models.Model):
