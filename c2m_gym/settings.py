420135bad7aa891e8e01eb326c1698a8e48b586d
MJ <mikey.j.1228@gmail.com>
2024-07-10 14:19:42 +0900

    Delete the messages override in settings entirely

--- c2m_gym/settings.py
+++ c2m_gym/settings.py
@@ -62,15 +62,6 @@ INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
 ]
 
-from django.contrib.messages import constants as message_constants
-
-MESSAGE_TAGS = {
-    message_constants.DEBUG: 'debug',
-    message_constants.INFO: 'info',
-    message_constants.SUCCESS: 'success',
-    message_constants.WARNING: 'warning',
-    message_constants.ERROR: 'danger',
-}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
